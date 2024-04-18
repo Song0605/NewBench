46759dfe7c2162fb2b8f1e2e7b71e08dd9d6fa0a
ChenSong <chensong@labauto.com.cn>
2024-04-18 13:08:01 +0800

    在主分支中去除Hub

--- NewBench.Core/Hub/ButtonHub.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NewBench.Core.Hub
-{
-    /// <summary>
-    /// 保存各类Button信息
-    /// </summary>
-    public class ButtonHub
-    {
-    }
-}
--- NewBench.Core/Interface/Instance/IHub`.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NewBench.Core.Interface.Instance
-{
-    public interface IHub<T> where T : class
-    {
-        bool Register(string key, T value);
-        bool Get(string key, out T? value);
-    }
-
-}
